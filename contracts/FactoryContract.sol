afe152dfd62659504c10344cf568109c2adda988
zaryab2000 <zaryabafser2000@gmail.com>
2020-08-24 17:58:49 +0530

    added newMovieContract

--- contracts/FactoryContract.sol
+++ contracts/FactoryContract.sol
@@ -10,4 +10,13 @@ contract FactoryContract {
 
 	mapping(string => address) public MovieToContracts;
 
+	function newMovieContract(string memory _movieName) public{
+		MovieContract instance = new MovieContract(_movieName,msg.sender);
+		recentContract = address(instance);
+		contractCount++;
+		contractContainer.push(address(instance));
+		MovieToContracts[_movieName] = address(instance);
+
+	}
+
 }
