c7eacb55281accdf91c1ed84c44d9d2f575fbca6
zaryab2000 <zaryabafser2000@gmail.com>
2020-08-24 18:33:33 +0530

    implementing ERC20 Interface

--- contracts/TokenFactory.sol
+++ contracts/TokenFactory.sol
@@ -61,4 +61,63 @@ pragma solidity ^0.5.1;
           TotalSupply = _initalSupply;
       } 
 
+      function totalSupply() public returns (uint256){
+         return TotalSupply;
+      }
+
+      // What is the balance of a particular account?
+      function balanceOf(address _owner) public returns (uint256 balance) {
+         return balances[_owner];
+      }
+
+      // Transfer the balance from owner's account to another account
+      function transfer(address _to, uint256 _amount) public returns (bool success) {
+         if (balances[msg.sender] >= _amount
+              && _amount > 0
+              && balances[_to] + _amount > balances[_to]) {
+              balances[msg.sender] -= _amount;
+              balances[_to] += _amount;
+              emit Transfer(msg.sender, _to, _amount);
+              return true;
+          } else {
+              return false;
+         }
+      }
+
+      // Send _value amount of tokens from address _from to address _to
+    
+      function transferFrom(
+          address _from,
+          address _to,
+         uint256 _amount
+    ) public returns (bool success) {
+       if (balances[_from] >= _amount
+            && allowed[_from][msg.sender] >= _amount
+           && _amount > 0
+            && balances[_to] + _amount > balances[_to]) {
+           balances[_from] -= _amount;
+           allowed[_from][msg.sender] -= _amount;
+            balances[_to] += _amount;
+             emit Transfer(_from, _to, _amount);
+             return true;
+        } else {
+            return false;
+         }
+     }
+
+       // Allow _spender to withdraw from your account, multiple times, up to the _value amount.
+     // If this function is called again it overwrites the current allowance with _value.
+     function approve(address _spender, uint256 _amount) public returns (bool success) {
+         allowed[msg.sender][_spender] = _amount;
+         emit Approval(msg.sender, _spender, _amount);
+         return true;
+     }
+
+     function allowance(address _owner, address _spender) public returns (uint256 remaining) {
+         return allowed[_owner][_spender];
+     }
+
+
+
+}
  
