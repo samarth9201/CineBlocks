0fbb061225a69141e35912212a580f0528d17fc4
zaryab2000 <zaryabafser2000@gmail.com>
2020-08-25 13:17:35 +0530

    added tokenPrice argument

--- contracts/MovieContract.sol
+++ contracts/MovieContract.sol
@@ -4,16 +4,19 @@ import "./TokenFactory.sol";
 import "./SafeMath.sol";
 
 contract MovieContract {
-      using SafeMath for uint256;
+  using SafeMath for uint256;
 
-	  string public movieName;
-	  string public tokenName;
-	  uint256 public creationDate;
-	  uint256 public deadline;
-	  uint256 public totalSupply;
-	  address public owner;
-	  address public tokenAddress;
+  address public owner;
+  address public tokenAddress;
+  string public movieName;
+  string public tokenName;
+  uint256 public deadline;
+  uint256 public totalSupply;
+  uint256 public tokenPrice;
+  uint256 public creationDate;
+  uint256 public investorCount;
 
+	  
 TokenFactory public token;
 
 
@@ -32,6 +35,7 @@ constructor(string memory _movieName, address payable _movieCreator) public{
   struct investorDetails{
   	string name;
   	string contactDetails;
+    address investorAddress;
     uint256 tokensBought;
   }
 
@@ -54,11 +58,16 @@ constructor(string memory _movieName, address payable _movieCreator) public{
     movie.deadline = now + _timeInDays * 1 days;
   }
 
-  function  createMovieToken(string memory _symbol, string memory _name, uint256 _totalSupply) public onlyOwner{
+  function  createMovieToken(string memory _symbol, string memory _name, uint256 _totalSupply,uint256 _price) public onlyOwner{
       token = new TokenFactory(_symbol, _name, _totalSupply);
       tokenAddress = address(token);
       tokenName = _name;
       totalSupply = _totalSupply;
+      tokenPrice = _price;
+
   }
+  
+  
+  
   
 }
